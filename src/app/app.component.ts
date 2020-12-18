39af68f1e4a385d9baee8ac7dbaacbad54705298
Singh, Atin <atin@pragra.io>
2020-12-17 21:59:42 -0500

    Updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,4 +20,36 @@ export class AppComponent {
   }
 
 
+  master = [
+    {
+      picUrl: 'https://www.ntvtelugu.com/uploads/images/2019/06/image_big_464955cfe4cbeaf517.jpg',
+      author: 'Ritesh',
+      joke: 'IT make me dumb, I was smarter before smart phone',
+      btnText: 'Know more about Ritesh'
+   },
+   {
+    picUrl: 'https://m.media-amazon.com/images/M/MV5BZDQ1OGJjYmQtNWM2Yy00ZjU3LWE4YzktZTE0Y2ZlYjAzMzY1XkEyXkFqcGdeQXVyMjg3Njc5NTI@._V1_.jpg',
+    author: 'Richa',
+    joke: 'What’s the best thing about Switzerland? I don’t know, but the flag is a big plus.',
+    btnText: 'Know more about Richa'
+ },
+ {
+  picUrl: 'https://www.crictracker.com/wp-content/uploads/2020/05/Rohit-Sharma-2.jpg',
+  author: 'Rohit',
+  joke: 'What’s the best thing about Switzerland? I don’t know, but the flag is a big plus.',
+  btnText: 'Know more about Rohit'
+},
+{
+  picUrl: 'https://www.ntvtelugu.com/uploads/images/2019/06/image_big_464955cfe4cbeaf517.jpg',
+  author: 'Ritesh',
+  joke: 'IT make me dumb, I was smarter before smart phone',
+  btnText: 'Know more about Ritesh'
+}
+
+  ]
+
+  updateTitle(e:any){
+    this.title = e;
+  }
+
 }
--- src/app/jokes/joke/joke.component.ts
+++ src/app/jokes/joke/joke.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
     selector: 'app-joke',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
     styleUrls: ['joke.component.scss']
 })
 export class JokeComponent {
+
+    @Input()
+    data:{picUrl:string,author: string,joke:string,btnText:string } ={
+    author:'',
+    picUrl:'',
+    joke:'',
+    btnText:''
+    };
+
+    @Output()
+    authorName = new EventEmitter<string>();
+
+    onBtnClick() {
+        console.log(this.data.author)
+        this.authorName.emit(this.data.author);
+    }
 
 }
